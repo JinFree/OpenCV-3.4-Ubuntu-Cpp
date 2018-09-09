acf3a560cc773e7d344c06181169588c85fad66e
Jinyeong Wang <ytrqwe12@gmail.com>
2018-09-09 20:24:14 +0900

    modify parameter & include sudoku_image

--- 01_OpenCV_Basic/04_Threshold_Methods/OpenCV.cpp
+++ 01_OpenCV_Basic/04_Threshold_Methods/OpenCV.cpp
@@ -6,26 +6,59 @@ using namespace cv;
 int main(void) {
 	double timer;
 	string openPath = path + image;
-	Mat lennaColor = imageRead(openPath, IMREAD_COLOR);
-	imageShow("lennaColor", lennaColor);
+	Mat sudokuColor = imageRead(openPath, IMREAD_COLOR);
+	imageShow("sudokuColor", sudokuColor);
 
-	Mat lennaGray = imageRead(openPath, IMREAD_GRAYSCALE);
-	imageShow("lennaGray", lennaGray);
+	Mat sudokuGray = imageRead(openPath, IMREAD_GRAYSCALE);
+	imageShow("sudokuGray", sudokuGray);
 
 	timer = (double)getTickCount();
-	Mat ByPtr_Gray = thresholdByPtr(lennaGray, 128);
+	Mat ByPtr_Gray = thresholdByPtr(sudokuGray, 128);
 	timer = ((double)getTickCount() - timer) / getTickFrequency();
 	cout << "threshold gray time ptr =  " << timer << " sec" << endl;
 	imageShow("ByPtr_Gray", ByPtr_Gray);
 
 	timer = (double)getTickCount();
-	Mat ByPtr_Color = thresholdByPtr(lennaColor, 128);
+	Mat ByPtr_Color = thresholdByPtr(sudokuColor, 128);
 	timer = ((double)getTickCount() - timer) / getTickFrequency();
 	cout << "threshold color time ptr =  " << timer << " sec" << endl;
 	imageShow("ByPtr_Color", ByPtr_Color);
 
-	
+	timer = (double)getTickCount();
+	Mat ByCV_Gray = thresholdByCV(sudokuGray, 128);
+	timer = ((double)getTickCount() - timer) / getTickFrequency();
+	cout << "threshold gray time cv =  " << timer << " sec" << endl;
+	imageShow("ByCV_Gray", ByCV_Gray);
 
+	timer = (double)getTickCount();
+	Mat ByCV_Color = thresholdByCV(sudokuColor, 128);
+	timer = ((double)getTickCount() - timer) / getTickFrequency();
+	cout << "threshold color time cv =  " << timer << " sec" << endl;
+	imageShow("ByCV_Color", ByCV_Color);
+
+	destroyAllWindows();
+
+	Mat CV_Gray_THRESH_BINARY = thresholdByCV(sudokuGray, 128, 255, THRESH_BINARY);
+	imageShow("CV_Gray_THRESH_BINARY", CV_Gray_THRESH_BINARY);
+
+	Mat CV_Gray_THRESH_BINARY_INV = thresholdByCV(sudokuGray, 128, 255, THRESH_BINARY_INV);
+	imageShow("CV_Gray_THRESH_BINARY_INV", CV_Gray_THRESH_BINARY_INV);
+
+	Mat CV_Gray_THRESH_TRUNC = thresholdByCV(sudokuGray, 128, 255, THRESH_TRUNC);
+	imageShow("CV_Gray_THRESH_TRUNC", CV_Gray_THRESH_TRUNC);
+
+	Mat CV_Gray_THRESH_TOZERO = thresholdByCV(sudokuGray, 128, 255, THRESH_TOZERO);
+	imageShow("CV_Gray_THRESH_TOZERO", CV_Gray_THRESH_TOZERO);
+
+	Mat CV_Gray_THRESH_TOZERO_INV = thresholdByCV(sudokuGray, 128, 255, THRESH_TOZERO_INV);
+	imageShow("CV_Gray_THRESH_TOZERO_INV", CV_Gray_THRESH_TOZERO_INV);
+
+	destroyAllWindows();
+
+	Mat CV_Gray_THRESH_BINARY = thresholdByCV(sudokuGray, 128, 255, THRESH_BINARY);
+	imageShow("CV_Gray_THRESH_BINARY", CV_Gray_THRESH_BINARY);
+
+	
 	destroyAllWindows();
 
 	return 0;
--- 01_OpenCV_Basic/04_Threshold_Methods/OpenCV.h
+++ 01_OpenCV_Basic/04_Threshold_Methods/OpenCV.h
@@ -1,18 +1,20 @@
 #include <iostream>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
 using namespace std;
 using namespace cv;
 
-string path = "../../Data/Lenna_Images/";
-string image = "Lenna.png";
+string path = "../../Data/";
+string image = "sudoku-original.jpg";
 
 Mat imageRead(string openPath, int flag = IMREAD_UNCHANGED);
 void imageShow(string imageName, Mat image, int flag = CV_WINDOW_NORMAL);
-Mat thresholdByAt(Mat image, uchar threshold = 128);
-Mat thresholdByPtr(Mat image, uchar threshold = 128);
-Mat thresholdByData(Mat image, uchar threshold = 128);
+Mat thresholdByCV(Mat image, double thresh = 128, double maxval = 255, int type = THRESH_BINARY);
+Mat thresholdByAt(Mat image, uchar thresh = 128);
+Mat thresholdByPtr(Mat image, uchar thresh = 128);
+Mat thresholdByData(Mat image, uchar thresh = 128);
 
 Mat imageRead(string openPath, int flag) {
 	Mat image = imread(openPath, flag);
@@ -32,13 +34,19 @@ void imageShow(string imageName, Mat image, int flag) {
     imshow(imageName, image);
 	waitKey(0);
 }
-Mat thresholdByAt(Mat image, uchar threshold) {
+
+Mat thresholdByCV(Mat image, double thresh, double maxval, int type) {
+	Mat result;
+	threshold(image, result, thresh, maxval, type);
+	return result;
+}
+Mat thresholdByAt(Mat image, uchar thresh) {
 	int i,j;
 	if(image.type() == CV_8UC1) {
 		for( j = 0 ; j < image.rows ; j++) {
 			for ( i = 0 ; i < image.cols ; i++ ) {
 				uchar value = image.at<uchar>(j,i);
-				value = value > threshold ? 255 : 0;
+				value = value > thresh ? 255 : 0;
 				image.at<uchar>(j,i) = value;
 			}
 		}
@@ -49,7 +57,7 @@ Mat thresholdByAt(Mat image, uchar threshold) {
 				Vec3b value = image.at<Vec3b>(j, i);
 				int c;
 				for(c = 0 ; c < 3 ; c++) {
-					value[c] = value[c] > threshold ? 255 : 0;
+					value[c] = value[c] > thresh ? 255 : 0;
 				}
 				image.at<Vec3b>(j,i) = value;
 			}
@@ -57,14 +65,14 @@ Mat thresholdByAt(Mat image, uchar threshold) {
 	}
 	return image;
 }
-Mat thresholdByPtr(Mat image, uchar threshold) {
+Mat thresholdByPtr(Mat image, uchar thresh) {
 	int i,j;
 	if(image.type() == CV_8UC1) {
 		for( j = 0 ; j < image.rows ; j++) {
 			uchar* image_pointer = image.ptr<uchar>(j);
 			for ( i = 0 ; i < image.cols ; i++ ) {
 				uchar value = image_pointer[i];
-				value = value > threshold ? 255 : 0;
+				value = value > thresh ? 255 : 0;
 				image_pointer[i] = value;
 			}
 		}
@@ -76,7 +84,7 @@ Mat thresholdByPtr(Mat image, uchar threshold) {
 				int c;
 				for(c = 0 ; c < 3 ; c++) {
 					uchar value = image_pointer[i*3+c];
-					value = value > threshold ? 255 : 0;
+					value = value > thresh ? 255 : 0;
 					image_pointer[i*3+c] = value;
 				}
 			}
@@ -84,14 +92,14 @@ Mat thresholdByPtr(Mat image, uchar threshold) {
 	}
 	return image;
 }
-Mat thresholdByData(Mat image, uchar threshold) {
+Mat thresholdByData(Mat image, uchar thresh) {
 	uchar *image_data = image.data;
 	int i,j;
 	if(image.type() == CV_8UC1) {
 		for( j = 0 ; j < image.rows ; j++) {
 			for ( i = 0 ; i < image.cols ; i++ ) {
 				uchar value = image_data[j * image.cols + i];
-				value = value > threshold ? 255 : 0;
+				value = value > thresh ? 255 : 0;
 				image_data[j * image.cols + i] = value;
 			}
 		}
@@ -102,7 +110,7 @@ Mat thresholdByData(Mat image, uchar threshold) {
 				int c;
 				for(c = 0 ; c < 3 ; c++) {
 					uchar value = image_data[(j * image.cols + i) * 3 + c];
-					value = value > threshold ? 255 : 0;
+					value = value > thresh ? 255 : 0;
 					image_data[(j * image.cols + i) * 3 + c] = value;
 				}
 			}
