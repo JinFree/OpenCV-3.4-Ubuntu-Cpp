e66e0af130baed4eacc0c9e25a5b748701558a59
Jinyeong Wang <ytrqwe12@gmail.com>
2018-09-09 20:23:37 +0900

    modify parameter

--- 01_OpenCV_Basic/03_Mat_Element/OpenCV.h
+++ 01_OpenCV_Basic/03_Mat_Element/OpenCV.h
@@ -10,9 +10,9 @@ string image = "Lenna.png";
 
 Mat imageRead(string openPath, int flag = IMREAD_UNCHANGED);
 void imageShow(string imageName, Mat image, int flag = CV_WINDOW_NORMAL);
-Mat thresholdByAt(Mat image, uchar threshold = 128);
-Mat thresholdByPtr(Mat image, uchar threshold = 128);
-Mat thresholdByData(Mat image, uchar threshold = 128);
+Mat thresholdByAt(Mat image, uchar thresh = 128);
+Mat thresholdByPtr(Mat image, uchar thresh = 128);
+Mat thresholdByData(Mat image, uchar thresh = 128);
 
 Mat imageRead(string openPath, int flag) {
 	Mat image = imread(openPath, flag);
@@ -32,13 +32,13 @@ void imageShow(string imageName, Mat image, int flag) {
     imshow(imageName, image);
 	waitKey(0);
 }
-Mat thresholdByAt(Mat image, uchar threshold) {
+Mat thresholdByAt(Mat image, uchar thresh) {
 	int i,j;
 	if(image.type() == CV_8UC1) {
 		for( j = 0 ; j < image.rows ; j++) {
 			for ( i = 0 ; i < image.cols ; i++ ) {
 				uchar value = image.at<uchar>(j,i);
-				value = value > threshold ? 255 : 0;
+				value = value > thresh ? 255 : 0;
 				image.at<uchar>(j,i) = value;
 			}
 		}
@@ -49,7 +49,7 @@ Mat thresholdByAt(Mat image, uchar threshold) {
 				Vec3b value = image.at<Vec3b>(j, i);
 				int c;
 				for(c = 0 ; c < 3 ; c++) {
-					value[c] = value[c] > threshold ? 255 : 0;
+					value[c] = value[c] > thresh ? 255 : 0;
 				}
 				image.at<Vec3b>(j,i) = value;
 			}
@@ -57,14 +57,14 @@ Mat thresholdByAt(Mat image, uchar threshold) {
 	}
 	return image;
 }
-Mat thresholdByPtr(Mat image, uchar threshold) {
+Mat thresholdByPtr(Mat image, uchar thresh) {
 	int i,j;
 	if(image.type() == CV_8UC1) {
 		for( j = 0 ; j < image.rows ; j++) {
 			uchar* image_pointer = image.ptr<uchar>(j);
 			for ( i = 0 ; i < image.cols ; i++ ) {
 				uchar value = image_pointer[i];
-				value = value > threshold ? 255 : 0;
+				value = value > thresh ? 255 : 0;
 				image_pointer[i] = value;
 			}
 		}
@@ -76,7 +76,7 @@ Mat thresholdByPtr(Mat image, uchar threshold) {
 				int c;
 				for(c = 0 ; c < 3 ; c++) {
 					uchar value = image_pointer[i*3+c];
-					value = value > threshold ? 255 : 0;
+					value = value > thresh ? 255 : 0;
 					image_pointer[i*3+c] = value;
 				}
 			}
@@ -84,14 +84,14 @@ Mat thresholdByPtr(Mat image, uchar threshold) {
 	}
 	return image;
 }
-Mat thresholdByData(Mat image, uchar threshold) {
+Mat thresholdByData(Mat image, uchar thresh) {
 	uchar *image_data = image.data;
 	int i,j;
 	if(image.type() == CV_8UC1) {
 		for( j = 0 ; j < image.rows ; j++) {
 			for ( i = 0 ; i < image.cols ; i++ ) {
 				uchar value = image_data[j * image.cols + i];
-				value = value > threshold ? 255 : 0;
+				value = value > thresh ? 255 : 0;
 				image_data[j * image.cols + i] = value;
 			}
 		}
@@ -102,7 +102,7 @@ Mat thresholdByData(Mat image, uchar threshold) {
 				int c;
 				for(c = 0 ; c < 3 ; c++) {
 					uchar value = image_data[(j * image.cols + i) * 3 + c];
-					value = value > threshold ? 255 : 0;
+					value = value > thresh ? 255 : 0;
 					image_data[(j * image.cols + i) * 3 + c] = value;
 				}
 			}
