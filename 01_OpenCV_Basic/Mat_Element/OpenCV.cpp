fabca247b5248297e9b51a21a584c9a8f5e62e87
Jinyeong Wang <ytrqwe12@gmail.com>
2018-09-09 17:57:03 +0900

    Mat_Element

--- 01_OpenCV_Basic/Mat_Element/OpenCV.cpp
+++ 01_OpenCV_Basic/Mat_Element/OpenCV.cpp
@@ -5,12 +5,29 @@ using namespace cv;
 
 int main(void) {
 	string openPath = path + image;
+	Mat lennaColor = imageRead(openPath, IMREAD_COLOR);
+	imageShow("lennaColor", lennaColor);
+
 	Mat lennaGray = imageRead(openPath, IMREAD_GRAYSCALE);
 	imageShow("lennaGray", lennaGray);
 
-	Mat ByAt = thresholdByAt(lennaGray, 128);
-	imageShow("ByAt", ByAt);
+	Mat ByAt_Gray = thresholdByAt(lennaGray, 128);
+	imageShow("ByAt_Gray", ByAt_Gray);
 
+	Mat ByAt_Color = thresholdByAt(lennaColor, 128);
+	imageShow("ByAt_Color", ByAt_Color);
+
+	Mat ByPtr_Gray = thresholdByPtr(lennaGray, 128);
+	imageShow("ByPtr_Gray", ByPtr_Gray);
+
+	Mat ByPtr_Color = thresholdByPtr(lennaColor, 128);
+	imageShow("ByPtr_Color", ByPtr_Color);
+
+	Mat ByData_Gray = thresholdByData(lennaGray, 128);
+	imageShow("ByData_Gray", ByData_Gray);
+
+	Mat ByData_Color = thresholdByData(lennaColor, 128);
+	imageShow("ByData_Color", ByData_Color);
 
 	destroyAllWindows();
 
--- 01_OpenCV_Basic/Mat_Element/OpenCV.h
+++ 01_OpenCV_Basic/Mat_Element/OpenCV.h
@@ -12,7 +12,7 @@ Mat imageRead(string openPath, int flag = IMREAD_UNCHANGED);
 void imageShow(string imageName, Mat image, int flag = CV_WINDOW_NORMAL);
 Mat thresholdByAt(Mat image, uchar threshold = 128);
 Mat thresholdByPtr(Mat image, uchar threshold = 128);
-Mat thresholdBtData(Mat image, uchar threshold = 128);
+Mat thresholdByData(Mat image, uchar threshold = 128);
 Mat imageRead(string openPath, int flag) {
 	Mat image = imread(openPath, flag);
 	if(image.empty()) {
@@ -34,20 +34,78 @@ void imageShow(string imageName, Mat image, int flag) {
 Mat thresholdByAt(Mat image, uchar threshold) {
 	int i,j;
 	if(image.type() == CV_8UC1) {
-		for ( i = 0 ; i < image.cols() ; i++ ) {
-			for( j = 0 ; j < image.rows() ; j++) {
+		for( j = 0 ; j < image.rows ; j++) {
+			for ( i = 0 ; i < image.cols ; i++ ) {
 				uchar value = image.at<uchar>(j,i);
 				value = value > threshold ? 255 : 0;
 				image.at<uchar>(j,i) = value;
 			}
 		}
 	}
+	else if(image.type() == CV_8UC3) {
+		for( j = 0 ; j < image.rows ; j++) {
+			for ( i = 0 ; i < image.cols ; i++ ) {
+				Vec3b value = image.at<Vec3b>(j, i);
+				int c;
+				for(c = 0 ; c < 3 ; c++) {
+					value[c] = value[c] > threshold ? 255 : 0;
+				}
+				image.at<Vec3b>(j,i) = value;
+			}
+		}
+	}
 	return image;
 }
 Mat thresholdByPtr(Mat image, uchar threshold) {
-
+	int i,j;
+	if(image.type() == CV_8UC1) {
+		for( j = 0 ; j < image.rows ; j++) {
+			uchar* image_pointer = image.ptr<uchar>(j);
+			for ( i = 0 ; i < image.cols ; i++ ) {
+				uchar value = image_pointer[i];
+				value = value > threshold ? 255 : 0;
+				image_pointer[i] = value;
+			}
+		}
+	}
+	else if(image.type() == CV_8UC3) {
+		for( j = 0 ; j < image.rows ; j++) {
+			uchar* image_pointer = image.ptr<uchar>(j);
+			for ( i = 0 ; i < image.cols ; i++ ) {
+				int c;
+				for(c = 0 ; c < 3 ; c++) {
+					uchar value = image_pointer[i*3+c];
+					value = value > threshold ? 255 : 0;
+					image_pointer[i*3+c] = value;
+				}
+			}
+		}
+	}
 	return image;
 }
-Mat thresholdBtData(Mat image, uchar threshold) {
+Mat thresholdByData(Mat image, uchar threshold) {
+	uchar *image_data = image.data;
+	int i,j;
+	if(image.type() == CV_8UC1) {
+		for( j = 0 ; j < image.rows ; j++) {
+			for ( i = 0 ; i < image.cols ; i++ ) {
+				uchar value = image_data[j * image.cols + i];
+				value = value > threshold ? 255 : 0;
+				image_data[j * image.cols + i] = value;
+			}
+		}
+	}
+	else if(image.type() == CV_8UC3) {
+		for( j = 0 ; j < image.rows ; j++) {
+			for ( i = 0 ; i < image.cols ; i++ ) {
+				int c;
+				for(c = 0 ; c < 3 ; c++) {
+					uchar value = image_data[(j * image.cols + i) * 3 + c];
+					value = value > threshold ? 255 : 0;
+					image_data[(j * image.cols + i) * 3 + c] = value;
+				}
+			}
+		}
+	}
 	return image;
 }
