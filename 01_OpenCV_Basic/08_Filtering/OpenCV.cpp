1c61c7e0fe7e9a38eb22267a5e968401e18ea741
Jinyeong Wang <ytrqwe12@gmail.com>
2018-09-11 08:41:52 +0900

    Some modification to course material

--- 01_OpenCV_Basic/08_Filtering/OpenCV.cpp
+++ 01_OpenCV_Basic/08_Filtering/OpenCV.cpp
@@ -4,6 +4,10 @@ using namespace std;
 using namespace cv;
 
 int main(void) {
-    
+    string openPath = path + image;
+	Mat LennaGray = imageRead(openPath, IMREAD_GRAYSCALE);
+
+	destroyAllWindows();
+
 	return 0;
 }
