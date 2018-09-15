2f4c69412047556af2d341e4f0b89ca6ccaabacc
Jinyeong Wang <ytrqwe12@gmail.com>
2018-09-16 00:20:00 +0900

    HoughLinesP to detect Lanes

---
+++ 01_OpenCV_Basic/12_HoughLinesP/OpenCV.cpp
@@ -0,0 +1,31 @@
+#include "OpenCV.h"
+
+using namespace std;
+using namespace cv;
+
+int main(void) {
+    Mat roadBGR = imageRead(path + roadImage, IMREAD_COLOR);
+    imageShow("roadBGR", roadBGR);
+
+    Mat roadGray = convertColor(roadBGR, CV_BGR2GRAY);
+    imageShow("roadGray", roadGray);
+
+    Mat roadROI2 = trapezoidalROI(roadGray, 0.35, 0.6, -0.1, 0.95);
+    imageShow("roadROI2", roadROI2);
+
+    Mat roadCanny = cannyEdge(roadROI2, 50, 100);
+    imageShow("roadCanny", roadCanny);
+
+    Mat roadROI = trapezoidalROI(roadCanny, 0.4, 0.65, 0.0, 0.9);
+    imageShow("roadROI", roadROI);
+
+    Mat roadLines = drawHoughLinesP(roadROI);
+    imageShow("roadLines", roadLines);
+
+    Mat roadSum = weightedSum(roadLines, roadBGR);
+    imageShow("roadSum", roadSum);
+
+    destroyAllWindows();
+
+	return 0;
+}
